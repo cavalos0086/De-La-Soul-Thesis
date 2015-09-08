d7f856eb89e91bef25b7641cdeefc3ae011b94a4
Cristian E. Avalos <cavalos0086@gmail.com>
2015-09-08 10:51:15 -0700

    added temp object containing data for simulation. If error, go back to this commit

--- public/app/filter/filter.js
+++ public/app/filter/filter.js
@@ -2,8 +2,8 @@ angular.module('soundGlomerate.filter', ['soundGlomerate.results'])
 
 .controller('FilterController', function ($scope, Filter) {
   $scope.genresEB = Filter.genresEB;
+  $scope.tempData = Filter.resultsObj;
   $scope.checkedVal = {};
-
   
 });
 
